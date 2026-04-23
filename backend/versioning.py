649e37a6f9f45003bb656d6746a8badb116dc772
Yury <mackto@live.ru>
2026-04-23 17:26:29 +0500

    Bump version to 2.8.1

--- backend/versioning.py
+++ backend/versioning.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import re
 from typing import Any
 
-PROJECT_VERSION = "2.8.0"
+PROJECT_VERSION = "2.8.1"
 RELEASE_TRACK = "stable"
 DEFAULT_UPDATE_PROVIDER = "github_releases"
 DEFAULT_RELEASE_CHANNEL = "stable"
